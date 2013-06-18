db73f72b87c5431468160e024eee8e2c8087b255
hanlee0707 <han.lee0707@gmail.com>
2013-06-18 15:23:41 +0900

    added relationships.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   attr_accessible :account_create_time, :create_account_sent_at, :create_account_token, :email, :first_name, :last_name, :password_digest, :password_reset_sent_at, :password_reset_token
+
+  has_many :employee_positions, dependent: :destroy
+  has_many :employee_position_types, :through => :employee_positions
+  VALID_PASSWORD_REGEX = /^[\S]+$/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 end
--- app/models/employee_positions.rb
+++ app/models/employee_positions.rb
@@ -1,3 +1,4 @@
 class EmployeePositions < ActiveRecord::Base
   attr_accessible :employee_id, :position
+  belongs_to :employee
 end
