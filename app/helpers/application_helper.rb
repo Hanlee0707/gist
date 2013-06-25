dd425ced8b30cfd4be4ce5a93501599731f501cf
hanlee0707 <han.lee0707@gmail.com>
2013-06-26 01:00:55 +0900

    organized files.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     new_object = f.object.send(association).klass.new(component_type_id: component_type_id, template: value)
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
+      render("articles/components/" + association.to_s.singularize + "_fields", f: builder)
     end
     radio_button(component_type, radio_option, value, data: {id: id, fields: fields.gsub("\n", "")})
   end
@@ -16,7 +16,7 @@ module ApplicationHelper
     new_object = f.object.send(association).klass.new(component_type_id: component_type_id, template: template)
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
+      render("articles/components/" + association.to_s.singularize + "_fields", f: builder)
     end
     link_to(name, '#', class: "add_components", data: {id: id, fields: fields.gsub("\n", "")})
   end
@@ -26,9 +26,13 @@ module ApplicationHelper
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       if template.nil? then
-        render(association.to_s.singularize +  "_fields", f: builder)
+        if association.to_s != "component_types" then
+          render("articles/components/" + association.to_s.singularize +  "_fields", f: builder)
+        else
+          render(association.to_s.singularize +  "_fields", f: builder)
+        end
       else
-        render(association.to_s.singularize + template.to_s + "_fields", f: builder)
+        render("articles/components/" + association.to_s.singularize + template.to_s + "_fields", f: builder)
       end
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
